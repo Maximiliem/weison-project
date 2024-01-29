564e46b1bdd1e1cd5a26e8142316ffb564606f8a
Maximiliem <maximiliem@yahoo.com>
2024-01-29 20:23:11 -0300

    Add:
    agentName storaged in index.html and input value goes to agentNameInput into clientform.html

--- Frontend/js/pickAgentNameInput.js
+++ Frontend/js/pickAgentNameInput.js
@@ -7,15 +7,19 @@ document.addEventListener('DOMContentLoaded', function() {
         agentNameInput.addEventListener('input', function() {
             localStorage.setItem('inputAgentName', agentNameInput.value);
         });
+
+        // Recupera el valor del localStorage y establece el campo inputAgentName
+        const storedAgentName = localStorage.getItem('inputAgentName');
+        if (storedAgentName) {
+            agentNameInput.value = storedAgentName;
+        }
     }
 
     // Para la segunda página (clientform.html)
     const agentNameInputOnForm = document.getElementById('agentName');
 
-    // Recupera el valor del localStorage
-    const storedAgentName = localStorage.getItem('inputAgentName');
-
     // Si hay un valor almacenado, establece el campo agentName
+    const storedAgentName = localStorage.getItem('inputAgentName');
     if (storedAgentName) {
         agentNameInputOnForm.value = storedAgentName;
     }
